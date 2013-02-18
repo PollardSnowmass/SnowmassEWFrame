779b99fbc58d001bce58d322460e70701b05a21c
Chris <cspollard@gmail.com>
2013-02-18 13:39:50 +0100

    setup.sh for osx.

--- Generator/setup.sh
+++ Generator/setup.sh
@@ -4,3 +4,5 @@ export PYTHIA8=~/Programming/pythia
 export PYTHIA8DATA=$PYTHIA8/xmldoc
 
 export LD_LIBRARY_PATH=$LHAPDF/lib:$PYTHIA8/lib:$FASTJET/lib:$LD_LIBRARY_PATH
+# for osx
+# export DYLD_LIBRARY_PATH=$LHAPDF/lib:$PYTHIA8/lib:$FASTJET/lib:$LD_LIBRARY_PATH
